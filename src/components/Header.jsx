bfdc6f01445dbbdde30bdf9ec79f4cad53782cc8
Meyvi <meyvim@gmail.com>
2025-02-24 22:33:09 -0800

    feat: add header and wedding pages

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => { 
+  const classString = window.location.href
+  return (
+  <p className='font-bodo text-2xl p-6'>
+    Teresa and Henry
+  </p>
+)}
+
+export default Header
---
+++ src/pages/the-wedding.jsx
@@ -0,0 +1,21 @@
+const WeddingPage = () => (
+  <>
+    <div className="bg-black align-center text-center mt-20 m-9 block">
+      <p className="font-bodo spacing-20 align-middle text-4xl pt-10">
+        April 20, 2026
+      </p>
+    </div>
+    <div className="bg-white h-screen pt-[8rem] pl-10 pr-10 pt-20 pb-20">
+      <p className="font-bodo text-black text-xl">CEREMONY</p>
+      <p className="font-bodo text-black pt-2">Begins Promptly at 4:00 PM at JW Marriott Las Vegas Resort & Spa</p>
+      <p className="font-bodo text-black pt-2">221 N Rampart Blvd, Las Vegas, NV 89145</p>
+      <p className="font-bodo text-black text-xl pt-10">COCKTAILS</p>
+      <p className="font-bodo text-black pt-2">Immediately Following Ceremony</p>
+      <p className="font-bodo text-black text-xl pt-10">DRESSCODE</p>
+      <p className="font-bodo text-black pt-2">Evening formal</p>
+    </div>
+  </>
+)
+
+export default WeddingPage
+ 
