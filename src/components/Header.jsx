0720b95f41e36819645e729cef1a78e4dcc0121d
Meyvi <meyvim@gmail.com>
2025-02-24 22:37:10 -0800

    remove window constant, not needed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,5 @@
-const Header = () => { 
-  const classString = window.location.href
-  return (
-  <p className='font-bodo text-2xl p-6'>
+const Header = () => <p className='font-bodo text-2xl p-6'>
     Teresa and Henry
   </p>
-)}
 
 export default Header
--- src/pages/the-wedding.jsx
+++ src/pages/the-wedding.jsx
@@ -18,4 +18,3 @@ const WeddingPage = () => (
 )
 
 export default WeddingPage
- 
