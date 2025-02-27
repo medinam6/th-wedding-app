06441fe759b5f584a961e6f264cbe35de28fdfaa
Meyvi <52260481+medinam6@users.noreply.github.com>
2025-02-26 22:51:11 -0800

    Update _app.jsx
    add background

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -6,10 +6,14 @@ import 'src/app/globals.css'
 function MyApp({ Component, pageProps }) {
 	return (
 		<>
+		<div className='bg-black fixed w-screen'>
 			<Header /> 
 			<NavMenu />
+		</div>
+		<div className='pt-20'>
 			<Component {...pageProps} />
-		</>
+			</div>
+</>
     )
 }
 
