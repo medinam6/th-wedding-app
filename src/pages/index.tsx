9c7fb628202ded9ae69259a2e9bf70c1928eccb5
Meyvi <52260481+medinam6@users.noreply.github.com>
2025-02-26 22:51:55 -0800

    Update index.tsx
    adjust margin

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 
 
 const HomePage = () => (
-    <p className="font-bodo flex justify-center items-center h-screen">
+    <p className="font-bodo flex justify-center items-center h-screen -mt-20">
        <img src='/TH.png' className="h-half p-10"/>
     </p>)
 
