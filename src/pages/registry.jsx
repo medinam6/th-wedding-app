a07de3a41837b514b5e0b533f549cea8e375268e
medinam6 <meyvim@gmail.com>
2025-04-20 21:07:24 -0700

    add registry

--- src/pages/registry.jsx
+++ src/pages/registry.jsx
@@ -4,8 +4,11 @@ const Registry = () => (
       Your presence at our wedding is the greatest gift of all!
     </p>
     <p className="font-bodo text-xl p-6 text-center">
-      If you would like to bless us with a gift, we would be grateful for a contribution to help us
-      start our next chapter together.
+      If you would like to give something, we would be grateful for a contribution to help us start
+      our next chapter.
+      </p>
+      <p className="font-bodo text-l text-center">
+      You can send via cash or Zelle to hntfinallyeverafter@gmail.com. Thank you!
     </p>
   </div>
 )
