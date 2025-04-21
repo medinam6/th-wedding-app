f73f28367d1ff9725cc4d333d67525530a0d1603
medinam6 <meyvim@gmail.com>
2025-04-20 21:43:06 -0700

    add faq

--- src/pages/registry.jsx
+++ src/pages/registry.jsx
@@ -6,8 +6,8 @@ const Registry = () => (
     <p className="font-bodo text-xl p-6 text-center">
       If you would like to give something, we would be grateful for a contribution to help us start
       our next chapter.
-      </p>
-      <p className="font-bodo text-l text-center">
+    </p>
+    <p className="font-bodo text-l text-center">
       You can send via cash or Zelle to hntfinallyeverafter@gmail.com. Thank you!
     </p>
   </div>
