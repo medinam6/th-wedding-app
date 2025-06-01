a152d64d13cef3191ae8873d1ee0337d31fd4cd8
medinam6 <meyvim@gmail.com>
2025-05-31 22:02:38 -0700

    prettier

--- src/pages/the-wedding.jsx
+++ src/pages/the-wedding.jsx
@@ -10,7 +10,9 @@ const WeddingPage = () => (
       </p>
       <p className="font-bodo text-black pt-2">221 N Rampart Blvd, Las Vegas, NV 89145</p>
       <p className="font-bodo text-black pt-2">
-        <a href="/map" className="text-blue-600 hover:underline">Parking & Directions to The Lodge</a>
+        <a href="/map" className="text-blue-600 hover:underline">
+          Parking & Directions to The Lodge
+        </a>
       </p>
       <p className="font-bodo text-black text-xl pt-10">COCKTAILS</p>
       <p className="font-bodo text-black pt-2">Immediately Following Ceremony</p>
