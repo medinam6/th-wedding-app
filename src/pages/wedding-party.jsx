1c7cc3b426d35e7f028ca54d7c3b491608d48429
medinam6 <meyvim@gmail.com>
2025-07-12 23:48:42 -0700

    fix: privacy and registry

--- src/pages/wedding-party.jsx
+++ src/pages/wedding-party.jsx
@@ -4,10 +4,10 @@ const WeddingParty = () => (
   <div className="px-4">
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-8">
       {[
-        { name: 'Natasha Puckdee', role: 'Matron of Honor', image: '/natasha.jpeg' },
-        { name: 'Amanda Kanoknata', role: 'Bridesmaid', image: '/amanda.jpeg' },
-        { name: 'Meyvi Medina', role: 'Bridesmaid', image: '/meyvi.jpeg' },
-        { name: 'Jessica Villa-Senor Villa', role: 'Bridesmaid', image: '/jessica.jpeg' },
+        { name: 'Natasha', role: 'Matron of Honor', image: '/natasha.jpeg' },
+        { name: 'Amanda', role: 'Bridesmaid', image: '/amanda.jpeg' },
+        { name: 'Meyvi', role: 'Bridesmaid', image: '/meyvi.jpeg' },
+        { name: 'Jessica', role: 'Bridesmaid', image: '/jessica.jpeg' },
       ].map(member => (
         <div key={member.name} className="text-center">
           <Image
@@ -25,10 +25,10 @@ const WeddingParty = () => (
 
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-8">
       {[
-        { name: 'Estevan Enriquez', role: 'Best Man', image: '/estevan.jpeg' },
-        { name: 'Aries Caytuero', role: 'Groomsman', image: '/aries.jpeg' },
-        { name: 'Bob Morgan', role: 'Groomsman', image: '/bob.jpeg' },
-        { name: 'Frankie Valera', role: 'Groomsman', image: '/frankie.jpeg' },
+        { name: 'Estevan', role: 'Best Man', image: '/estevan.jpeg' },
+        { name: 'Aries', role: 'Groomsman', image: '/aries.jpeg' },
+        { name: 'Bob', role: 'Groomsman', image: '/bob.jpeg' },
+        { name: 'Frankie', role: 'Groomsman', image: '/frankie.jpeg' },
       ].map(member => (
         <div key={member.name} className="text-center">
           <Image
